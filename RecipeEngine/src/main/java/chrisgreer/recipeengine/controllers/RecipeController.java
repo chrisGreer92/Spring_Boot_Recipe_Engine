1dea14c2d271ca9d591c26adb8f249cee5e0951b
chrisGreer92 <chris.i.greer@gmail.com>
2025-09-01 20:34:17 +0100

    Create RecipeController Stub

---
+++ RecipeEngine/src/main/java/chrisgreer/recipeengine/controllers/RecipeController.java
@@ -0,0 +1,14 @@
+package chrisgreer.recipeengine.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recipes")
+@RequiredArgsConstructor
+public class RecipeController {
+
+
+}
