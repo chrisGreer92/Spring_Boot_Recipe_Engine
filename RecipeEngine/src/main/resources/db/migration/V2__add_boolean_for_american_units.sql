0dd10782977642a1348fad13e3e53a8ce09c7b07
chrisGreer92 <chris.i.greer@gmail.com>
2025-09-15 17:01:35 +0100

    Renamed boolean to is_non_uk_unit rather than is_american_unit for clarity
    N.B. Edited V2 migration which is bad practice but still in early stages so cleared and re migrated

--- RecipeEngine/src/main/resources/db/migration/V2__add_boolean_for_american_units.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE recipe_ingredient
-    ADD COLUMN is_american_unit BOOLEAN DEFAULT FALSE;
---
+++ RecipeEngine/src/main/resources/db/migration/V2__add_boolean_for_non_uk_units.sql
@@ -0,0 +1,2 @@
+ALTER TABLE recipe_ingredient
+    ADD COLUMN is_non_uk_unit BOOLEAN DEFAULT FALSE;
