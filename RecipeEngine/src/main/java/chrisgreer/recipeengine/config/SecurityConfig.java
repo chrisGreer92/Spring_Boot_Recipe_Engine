2961f5bab566fcf925ad5cd5c51bcbb409eab548
chrisGreer92 <chris.i.greer@gmail.com>
2025-09-17 13:46:44 +0100

    Add security config

---
+++ RecipeEngine/src/main/java/chrisgreer/recipeengine/config/SecurityConfig.java
@@ -0,0 +1,79 @@
+package chrisgreer.recipeengine.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${admin.username}") private String adminUsername;
+    @Value("${admin.password}") private String adminPassword;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        //Use stateless sessions (token based)
+        //Disable CSRF protection (cross site request forgery)
+        //Authorise which endpoints public/private
+        http
+                .cors(Customizer.withDefaults())
+                .sessionManagement(c ->
+                    c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(c -> c
+                    // Only GET for api is permitted
+                    .requestMatchers(HttpMethod.GET, "/recipes/**").permitAll()
+                    // Plus any static resources
+                    .requestMatchers("/*.html", "/*.js", "/*.css", "/*.ico").permitAll()
+                    // Everything else requires auth
+                    .anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());
+
+        return http.build();
+
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://chrisgreer.uk");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+    @Bean
+    UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        return new InMemoryUserDetailsManager(
+                User.withUsername(adminUsername)
+                        .password(encoder.encode(adminPassword))
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
+}
